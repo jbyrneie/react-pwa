c9a1eb9dcc4709212e7671653c746cd7cfd7df9b
Jack Byrne <jbyrne@glgroup.com>
2016-12-23 14:55:43 +0000

    Service Worker path, remove unused files

--- public/js/app.js
+++ public/js/app.js
@@ -231,7 +231,7 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-     .register('/service-worker.js')
+     .register('./assets/service-worker.js')
      .then(function() {
         console.log('Service Worker Registered');
       });

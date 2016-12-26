8c3ee56f689910297f95d2e51f0c02b6ce9fbf26
Jack Byrne <jbyrne@glgroup.com>
2016-12-26 13:46:11 +0000

    manifest start_url

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-var cacheName = 'weatherPWA-v2-1';
-var dataCacheName = 'weatherData-v2-1';
+var cacheName = 'weatherPWA-v2-2';
+var dataCacheName = 'weatherData-v2-2';
 var filesToCache = [
   './',
   './index.html',

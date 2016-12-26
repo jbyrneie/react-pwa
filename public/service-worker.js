f48a0155da1d299f00050befad4e7e1fab93b5da
Jack Byrne <jbyrne@glgroup.com>
2016-12-26 10:41:08 +0000

    cache paths

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,24 +3,24 @@ var dataCacheName = 'weatherData-v2';
 var filesToCache = [
   './',
   './index.html',
-  './assets./app.js',
-  './assets./localforage-1.4.0.js',
-  './styles./ud811.css',
-  './images./clear.png',
-  './images./cloudy-scattered-showers.png',
-  './images./cloudy.png',
-  './images./fog.png',
-  './images./ic_add_white_24px.svg',
-  './images./ic_refresh_white_24px.svg',
-  './images./partly-cloudy.png',
-  './images./rain.png',
-  './images./scattered-showers.png',
-  './images./sleet.png',
-  './images./snow.png',
-  './images./thunderstorm.png',
-  './images./wind.png'
+  './assets/app.js',
+  './assets/localforage-1.4.0.js',
+  './styles/ud811.css',
+  './images/clear.png',
+  './images/cloudy-scattered-showers.png',
+  './images/cloudy.png',
+  './images/fog.png',
+  './images/ic_add_white_24px.svg',
+  './images/ic_refresh_white_24px.svg',
+  './images/partly-cloudy.png',
+  './images/rain.png',
+  './images/scattered-showers.png',
+  './images/sleet.png',
+  './images/snow.png',
+  './images/thunderstorm.png',
+  './images/wind.png'
 ];
-var weatherAPIUrlBase = 'https:././publicdata-weather.firebaseio.com./';
+var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com./';
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
